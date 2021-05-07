0f327d8028ebb09752eeb69591058e4906d1243f
Hayden-coder <hayden377@hotmail.com>
2021-05-07 10:36:17 -0700

    Updates

---
+++ precache-manifest.338b2f149756bbe1ffa8093b43fe5f86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cfbe433e4c543f6f42ecd2afadd2279",
+    "url": "/react-deploy-/index.html"
+  },
+  {
+    "revision": "f0502039a5cf0d2ec136",
+    "url": "/react-deploy-/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "7d462b3b0fa2e84aff87",
+    "url": "/react-deploy-/static/css/main.cad86f24.chunk.css"
+  },
+  {
+    "revision": "f0502039a5cf0d2ec136",
+    "url": "/react-deploy-/static/js/2.9ce86cb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-/static/js/2.9ce86cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d462b3b0fa2e84aff87",
+    "url": "/react-deploy-/static/js/main.bf16d7b5.chunk.js"
+  },
+  {
+    "revision": "1d08f8086301afca997a",
+    "url": "/react-deploy-/static/js/runtime-main.3f003ed5.js"
+  }
+]);
